afd1910e64c042f94a28203ee9bb9bd5c2a77923
ABulger01 <Nyesha2001@gmail.com>
2025-05-08 04:47:26 +0000

    inserting table

--- main.cpp
+++ main.cpp
@@ -15,6 +15,11 @@ int main() {
 // This function will convert Fahrenheit to Celsius
 double fahrenheitToCelsius(int fahrenheit) {
     return (fahrenheit - 32_) * 5.0 / 9.0;
-  
+}
+// This function will display the table
+void displayTable() {
+    cout << "Fahrenheit\tCelsius\n";
+    cout << "-------------------\n";
+  "
 }
 }
