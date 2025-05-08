c64f6cf350ecf47bc766c9d253f9f6ce8851d844
ABulger01 <Nyesha2001@gmail.com>
2025-05-08 03:40:14 +0000

    Getting user input of the rectangle

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -12,6 +12,10 @@ bool askToContinue();
 int main () {
     do { 
         // Get the length and width of the rectangle from the user
+        double length = getPositiveInput("Enter the length of the rectangle: ");
+        double width = getPositiveInput("Enter the width of the rectangle: ");
+
+        // Calculate the perimeter and the area of the rectangle
         
 }
     
