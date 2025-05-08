a3361a4e8ca112e61f66d3a22bcd8a45e3fe8380
ABulger01 <Nyesha2001@gmail.com>
2025-05-08 04:51:52 +0000

    End of program

--- main.cpp
+++ main.cpp
@@ -14,12 +14,15 @@ int main() {
 }
 // This function will convert Fahrenheit to Celsius
 double fahrenheitToCelsius(int fahrenheit) {
-    return (fahrenheit - 32_) * 5.0 / 9.0;
+    return (fahrenheit - 32) * 5.0 / 9.0;
 }
 // This function will display the table
 void displayTable() {
     cout << "Fahrenheit\tCelsius\n";
-    cout << "-------------------\n";
-  "
+    cout << "------------------------\n";
+    for (int fahrenheit = 0; fahrenheit <= 20; fahrenheit++)
+        cout << fahrenheit << "\t\t" << fixed << setprecision(2) << fahrenheitToCelsius(fahrenheit) << endl;
+    
+  
 }
-}
+
