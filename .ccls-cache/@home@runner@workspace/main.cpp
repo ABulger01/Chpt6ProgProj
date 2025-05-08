cd97c626c8d9aeffc06e812b0001cf0bbfed40bb
ABulger01 <Nyesha2001@gmail.com>
2025-05-08 04:18:59 +0000

    added code description

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,58 +1,9 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-// The progam will calculate and display the perimeter and area of a rectangle with information provided by the user.
+// This program will determines which geographic regions within a major city (north, south, east, west, and central) had the fewest reported automobile accidents last year.
 
-// Function prototypes
-double getPositiveInput(string prompt);
-double calculatePerimeter(double length, double width);
-double calculateArea(double length, double width);
-bool askToContinue();
-
-int main () {
-    do { 
-        // Get the length and width of the rectangle from the user
-        double length = getPositiveInput("Enter the length of the rectangle: ");
-        double width = getPositiveInput("Enter the width of the rectangle: ");
-
-        // Calculate the perimeter and the area of the rectangle
-        double perimeter = calculatePerimeter(length, width);
-        double area = calculateArea(length, width);
-
-      // Display the results
-        cout << "The perimeter of the rectangle is: " << perimeter << endl;
-        cout << "The area of the rectangle is: " << area << endl;
-
-    } while (askToContinue());
-    return 0;
-}
-    // Funtion to get positive input from the user
-    double getPositiveInput(string prompt) {
-        double input;
-        do {
-            cout << prompt;
-            cin >> input;
-
-            if (input < 0) {
-                cout << "Invalid input. Please enter a postive number." << endl;
-            }
-        } while (input < 0);
-        return input;
-    }  
-    // Function to calculate the perimeter of the rectangle
-    double calculatePerimeter(double length, double width) {
-        return 2 * (length * width);
-    }
-    // Function to calculate the area of the rectangle
-    double calculateArea(double length, double width) {
-        return length * width;
-    }
-
-    // Function to ask the user if they want to continue
-    bool askToContinue() {
-        char choice;
-        cout << "Do you want to try another rectangle? (y/n): ";
-        cin >> choice;
-        return choice == 'y' || choice == 'Y';
-  }
-
+// Function protoypes
+int getAccidents(string region);
+void findLowest(int north, int south, int east,)
