463bfccb8d4f25a1825565eb845fd5f8da2d1764
ABulger01 <Nyesha2001@gmail.com>
2025-05-08 03:32:51 +0000

    added function prototypes

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -3,8 +3,14 @@ using namespace std;
 
 // The progam will calculate and display the perimeter and area of a rectangle with information provided by the user.
 
+// Function prototypes
+double getPositiveInput(string prompt);
+double calculatePerimeter(int length, int width);
+double calculateArea(int length, int width);
+bool askToContinue();
+
 int main () {
-    int length;
-    int width;
+    do {}
+}
     
 }
